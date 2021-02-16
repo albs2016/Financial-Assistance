29f9eafc73865480f51a83cb9bb778b7ca3bb67c
Connor Albright <connoralbright216@gmail.com>
2021-02-15 22:14:16 -0700

    Redid Code to make app work again

--- app/src/main/java/com/example/financialassistance/MainActivity.java
+++ app/src/main/java/com/example/financialassistance/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.financialassistance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int number = 1;
+
+
+    }
+
+    public void DebtButton(View view)
+    {
+        Intent intent = new Intent(this, DebtActivity.class);
+        startActivity(intent);
+    }
+
+    public void BudgetButton(View view)
+    {
+        Intent intent = new Intent(this, BudgetActivity.class);
+        startActivity(intent);
     }
 }
