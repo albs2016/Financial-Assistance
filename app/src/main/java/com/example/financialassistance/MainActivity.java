4bb243f3dbaad286f7eff54dca85adc9e87e696b
Connor Albright <connoralbright216@gmail.com>
2021-02-15 22:22:17 -0700

    Added Firebase

--- app/src/main/java/com/example/financialassistance/MainActivity.java
+++ app/src/main/java/com/example/financialassistance/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +18,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         int number = 1;
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
 
+        myRef.setValue("Hello, World!");
     }
 
     public void DebtButton(View view)
